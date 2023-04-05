1b77e957ad0ecc719ed9da8c0b294d9f50705212
Legofan0807 <83748124+Legofan0807@users.noreply.github.com>
2023-04-05 18:08:36 +0200

    seperated float and int variants of randon function

--- EngineSource/Engine/EngineRandom.h
+++ EngineSource/Engine/EngineRandom.h
@@ -2,6 +2,6 @@
 
 namespace Random
 {
-	int GetRandomNumber(int Min, int Max);
-	float GetRandomNumber(float Min, float Max);
+	int GetRandomInt(int Min, int Max);
+	float GetRandomFloat(float Min, float Max);
 }
