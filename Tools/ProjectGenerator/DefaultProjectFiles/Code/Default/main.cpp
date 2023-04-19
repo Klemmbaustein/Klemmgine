c40cf5ad7728b4af834f1981d64009025c77b9a2
Legofan0807 <83748124+Legofan0807@users.noreply.github.com>
2023-04-19 18:44:18 +0200

    main function is now cleaner

--- Tools/ProjectGenerator/DefaultProjectFiles/Code/Default/main.cpp
+++ Tools/ProjectGenerator/DefaultProjectFiles/Code/Default/main.cpp
@@ -9,55 +9,41 @@
 
 int Initialize(int argc, char** argv);
 
+void HandleError(std::string ErrorMessage)
+{
+	OS::SetConsoleWindowVisible(true);
+	SDL_DestroyWindow(Application::Window);
+	Sound::StopAllSounds();
+	Sound::End();
+	OS::ClearConsoleWindow();
+	std::cout << "Thrown uncaught exception at: " << Debugging::EngineStatus << ": \n";
+	std::cout << ErrorMessage;
+	std::cout << "\nPress enter to display engine log\n";
+	std::cin.get();
+	std::cout << "Engine Log:\n";
+	for (Log::Message i : Log::Messages)
+	{
+		std::cout << "    " <<
+			std::string(i.Amount != 0 ?
+				i.Text + std::string(" (x") + std::to_string(i.Amount + 1) + std::string(")") :
+				i.Text) << "\n";
+	}
+	std::cout << "\nPress enter to continue";
+	std::cin.get();
+}
+
 int main(int argc, char** argv)
 {
 	try
 	{
 		return Initialize(argc, argv);
-
 	}
 	catch (const std::exception& e)
 	{
-		OS::SetConsoleWindowVisible(true);
-		SDL_DestroyWindow(Application::Window);
-		Sound::StopAllSounds();
-		Sound::End();
-		OS::ClearConsoleWindow();
-		std::cout << "Thrown uncaught exception at: " << Debugging::EngineStatus << ": \n";
-		std::cout << e.what();
-		std::cout << "\nPress enter to display engine log\n";
-		std::cin.get();
-		std::cout << "Engine Log:\n";
-		for (Log::Message i : Log::Messages)
-		{
-			std::cout << "    " <<
-				std::string(i.Amount != 0 ?
-					i.Text + std::string(" (x") + std::to_string(i.Amount + 1) + std::string(")") :
-					i.Text) << "\n";
-		}
-		std::cout << "\nPress enter to continue";
-		std::cin.get();
+		HandleError(e.what());
 	}
 	catch (const char* e)
 	{
-		OS::SetConsoleWindowVisible(true);
-		SDL_DestroyWindow(Application::Window);
-		Sound::StopAllSounds();
-		Sound::End();
-		system("CLS");
-		std::cout << "Unknown error at: " << Debugging::EngineStatus << ": \n";
-		std::cout << e;
-		std::cout << "\nPress enter to display engine log\n";
-		std::cin.get();
-		std::cout << "Engine Log:\n";
-		for (Log::Message i : Log::Messages)
-		{
-			std::cout << "    " <<
-				std::string(i.Amount != 0 ?
-					i.Text + std::string(" (x") + std::to_string(i.Amount + 1) + std::string(")") :
-					i.Text) << "\n";
-		}
-		std::cout << "\nPress enter to continue";
-		std::cin.get();
+		HandleError(e);
 	}
 }
