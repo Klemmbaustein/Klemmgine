fdee9536335d8b45823b9ce9046a71cf97c0fb5d
Klemmbaustein <83748124+Klemmbaustein@users.noreply.github.com>
2023-08-27 14:07:56 +0200

    setup changes

--- setup-deps.ps1
+++ setup-deps.ps1
@@ -8,25 +8,25 @@ Write-Host "--- Finished building SDL2 ---"
 
 Write-Host "--- Configuring glew ---"
 cd Dependencies\glew-cmake
-cmake CMakeLists.txt
+cmake -S . -B Build/
 Write-Host "--- Building glew ---"
-msbuild libglew_static.vcxproj -nologo /p:Configuration=Release /p:Platform=x64
+msbuild Build\libglew_static.vcxproj -nologo /p:Configuration=Release /p:Platform=x64
 cd ..\..
 Write-Host "--- Finished building glew ---"
 
 Write-Host "--- Configuring assimp ---"
 cd Dependencies\assimp
-cmake CMakeLists.txt
+cmake -S . -B Build/
 Write-Host "--- Building assimp ---"
-msbuild code\assimp.vcxproj -nologo /p:Configuration=Release /p:Platform=x64
+msbuild Build\assimp.vcxproj -nologo /p:Configuration=Release /p:Platform=x64
 cd ..\..
 Write-Host "--- Finished building assimp ---"
 
 Write-Host "--- Configuring OpenAL ---"
 cd Dependencies\openal-soft
-cmake CMakeLists.txt
+cmake CMakeLists.txt -S . -B Build/
 Write-Host "--- Building OpenAL ---"
-msbuild OpenAL.vcxproj -nologo /p:Configuration=Release /p:Platform=x64
+msbuild Build/OpenAL.vcxproj -nologo /p:Configuration=Release /p:Platform=x64
 cd ..\..
 Write-Host "--- Finished building OpenAL ---"
 Write-Host "--- Finished setting up dependencies ---"
